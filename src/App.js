f09cf7bd18eeb15792261181a4e6cb785e8bfc2e
Victor Mendes <victorutdsim7@gmail.com>
2022-08-23 16:34:07 -0300

    Requisito 3 [completo]: componente Card.jsx

--- src/App.js
+++ src/App.js
@@ -1,30 +1,73 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 import './App.css';
 
 // passar multiplas props: https://www.youtube.com/watch?v=aWWIqdCc4GA
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: false,
+    hasTrunfo: false,
+    isSaveButtonDisabled: false,
+    onInputChange: () => { },
+    onSaveButtonClick: () => { },
+  };
+
   render() {
-    const data = {
-      cardName: '',
-      cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
-      cardImage: '',
-      cardRare: '',
-      cardTrunfo: false,
-      hasTrunfo: false,
-      isSaveButtonDisabled: false,
-      onInputChange: () => {},
-      onSaveButtonClick: () => {},
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.state;
+
+    const formData = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    };
+
+    const cardData = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
     };
 
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form { ...data } />
+        <Form { ...formData } />
+        <Card { ...cardData } />
       </div>
     );
   }
