1b4f721d2bce41cf2e705dbac2254acf6c638a1a
Victor Mendes <victorutdsim7@gmail.com>
2022-08-23 19:45:03 -0300

    Requisito 5 [completo]: funções de validação do formulário

--- src/App.js
+++ src/App.js
@@ -16,8 +16,42 @@ class App extends React.Component {
     cardRare: 'normal',
     cardTrunfo: false,
     hasTrunfo: false,
-    isSaveButtonDisabled: false,
-    onSaveButtonClick: () => { },
+    isSaveButtonDisabled: true,
+  };
+
+  validateAttributes = (arrOfAttributes) => {
+    const attributesSum = arrOfAttributes
+      .reduce((sum, elem) => Number(sum) + Number(elem));
+    const sumLimit = 210;
+    const validSum = attributesSum <= sumLimit;
+    const attributeMaxLimit = 90;
+    const attributesAreValid = arrOfAttributes
+      .every((element) => element >= 0 && element <= attributeMaxLimit);
+    return (validSum && attributesAreValid);
+  };
+
+  validateForm = () => {
+    const {
+      cardName,
+      cardImage,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    let textFieldsNotEmpty = false;
+    if (cardName && cardImage && cardDescription) {
+      textFieldsNotEmpty = true;
+    }
+    const attributes = [cardAttr1, cardAttr2, cardAttr3];
+    const attributesValid = this.validateAttributes(attributes);
+
+    if (textFieldsNotEmpty && attributesValid) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   };
 
   onInputChange = ({ target }) => {
@@ -27,7 +61,12 @@ class App extends React.Component {
       {
         [name]: value,
       },
+      this.validateForm,
     );
+  };
+
+  onSaveButtonClick = () => {
+
   };
 
   render() {
@@ -42,7 +81,6 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
-      onSaveButtonClick,
     } = this.state;
 
     const formData = {
@@ -56,7 +94,6 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
-      onSaveButtonClick,
     };
 
     const cardData = {
@@ -73,8 +110,12 @@ class App extends React.Component {
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form { ...formData } onInputChange={ this.onInputChange } />
-        <Card { ...cardData } onInputChange={ this.onInputChange } />
+        <Form
+          { ...formData }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
+        <Card { ...cardData } />
       </div>
     );
   }
