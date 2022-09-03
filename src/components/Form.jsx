eb88ab4c617c6227d8f66fdecda2c018511a0575
Victor Mendes <victorutdsim7@gmail.com>
2022-09-03 13:27:41 -0300

    correção definitiva descrição

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -45,7 +45,7 @@ class Form extends Component {
             data-testid="description-input"
             className="form-input"
             placeholder="descrição-da-carta"
-            maxLength={ 80 }
+            maxLength={ 70 }
           />
         </label>
         <label htmlFor="attribute-1" className="input-label">
